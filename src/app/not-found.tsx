8744e3ab9a302dbd91297a3b3d3fe691d1047e2d
Anthony jeamme <anthony@jeamme.fr>
2023-06-29 15:40:15 +0200

    Not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,23 @@
+import { classNameModule } from "@/utils/className/className";
+import styles from "./not-found.module.scss";
+import Link from "next/link";
+const className = classNameModule(styles);
+
 export default function NotFoundPage() {
   return (
-    <main>
-      <h1>404 - Page Not Found</h1>
+    <main {...className("NotFoundPage")}>
+      <div>
+        <img
+          {...className("brand")}
+          height={38}
+          width={138}
+          src="https://res.cloudinary.com/anthony-jeamme-stuff/image/upload/v1639411627/alteria-white.svg"
+          alt=""
+        />
+        <h1>Oups, cette page n{"'"}existe pas</h1>
+
+        <Link href="/">Retour à l{"'"}accueil</Link>
+      </div>
     </main>
   );
 }
